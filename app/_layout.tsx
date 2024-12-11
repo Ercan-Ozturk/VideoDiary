fa6efc66cb098d6103574b7e54b0bccbea56ec89
Ercan Öztürk <ercanozturk788@gmail.com>
2024-12-11 19:59:27 +0300

    gestures

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,12 @@
 import { Stack } from "expo-router";
 import { LogBox } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 LogBox.ignoreAllLogs(true);
 
 export default function RootLayout() {
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar style="light"></StatusBar>
       <Stack>
         <Stack.Screen
@@ -15,6 +16,6 @@ export default function RootLayout() {
 
         <Stack.Screen name="+not-found" options={{}}></Stack.Screen>
       </Stack>
-    </>
+    </GestureHandlerRootView>
   );
 }
